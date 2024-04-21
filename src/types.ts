af8cb80628b1def3cb79b03091d3abf03a6695eb
Mario González <mario@mariogl.com>
2024-04-21 08:23:59 +0200

    Initial commit

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export interface Student {
+  id: number;
+  name: string;
+}
+
+export interface Delivery {
+  id: number;
+  owner: string;
+  week: number;
+  frontRepoUrl: string;
+  frontProductionUrl: string;
+  backRepoUrl: string;
+  backProductionUrl: string;
+  sprint1TrelloUrl: string;
+  sprint2TrelloUrl: string;
+  firstTeammateName: string;
+  secondTeammateName: string;
+}
